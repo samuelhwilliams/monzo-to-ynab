8f7bad8bf4f3127106b24f4a0024c5f5138fd4d5
Samuel Williams <samuelhwilliams@gmail.com>
2021-04-29 14:38:06 +0100

    rework for heroku

--- main.py
+++ main.py
@@ -2,48 +2,52 @@ import os
 from typing import Tuple, Dict
 
 import flask
+from flask import request
 
 from monzo_to_ynab.ynab import YnabClient
 from monzo_to_ynab.monzo import MonzoClient
 from monzo_to_ynab.transactions import Transaction
-from monzo_to_ynab.utils import kms_decrypt
 
 
-KMS_SECRET_RESOURCE_NAME = os.environ["KMS_SECRET_RESOURCE_NAME"]  # GCP KMS key to decrypt secrets
-YNAB_TOKEN = kms_decrypt(
-    KMS_SECRET_RESOURCE_NAME, os.environ["BASE64_KMS_ENCRYPTED_YNAB_TOKEN"]
-)  # YNAB Personal Access Token
-YNAB_BUDGET_ID = os.environ["YNAB_BUDGET_ID"]  # Budget ID as found in the URL with YNAB
-YNAB_ACCOUNT_ID = os.environ["YNAB_ACCOUNT_ID"]  # Account ID under the specified YNAB Budget ID
-MONZO_ACCOUNT_ID = os.environ["MONZO_ACCOUNT_ID"]  # Only process transactions from this account
-MONZO_TOKEN = os.environ["MONZO_TOKEN"]  # Auth token provided to Monzo as the query param `token` in their webhook URL
+MONZO_TO_YNAB_CONFIG = os.environ['MONZO_TO_YNAB_CONFIG']
 
 
-def handle_monzo_transaction(transaction: Transaction) -> Tuple[str, int]:
-    client = YnabClient(token=YNAB_TOKEN)
+def handle_monzo_transaction(transaction: Transaction, ynab_token, ynab_budget_id, ynab_account_id) -> Tuple[str, int]:
+    client = YnabClient(token=ynab_token)
 
     transaction_id = client.find_existing_transaction(
-        budget_id=YNAB_BUDGET_ID, account_id=YNAB_ACCOUNT_ID, transaction=transaction
+        budget_id=ynab_budget_id, account_id=ynab_account_id, transaction=transaction
     )
     if transaction_id:
         updated_transaction = client.update_transaction(
-            budget_id=YNAB_BUDGET_ID, account_id=YNAB_ACCOUNT_ID, transaction_id=transaction_id, transaction=transaction
+            budget_id=ynab_budget_id, account_id=ynab_account_id, transaction_id=transaction_id, transaction=transaction
         )
 
         return f"Updated existing transaction: {updated_transaction}", 200
 
     else:
         new_transaction = client.create_transaction(
-            budget_id=YNAB_BUDGET_ID, account_id=YNAB_ACCOUNT_ID, transaction=transaction
+            budget_id=ynab_budget_id, account_id=ynab_account_id, transaction=transaction
         )
 
         return f"Created new transaction: {new_transaction}", 200
 
 
-def main(request: flask.Request) -> Tuple[str, int]:
-    if request.args.get("token") != MONZO_TOKEN:
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def main() -> Tuple[str, int]:
+    token = request.args.get("token")
+    if token not in MONZO_TO_YNAB_CONFIG:
         print("Unauthorized", 401)
         return "Unauthorized", 401
+    
+    config = MONZO_TO_YNAB_CONFIG[token]
+    ynab_token = config['ynab-token']
+    ynab_budget_id = config['ynab-budget-id']
+    ynab_account_id = config['ynab-account-id']
+    monzo_account_id = config['monzo-account-id']
 
     data: Dict = request.json
     print(f"Incoming data: {data}")
@@ -57,7 +61,7 @@ def main(request: flask.Request) -> Tuple[str, int]:
         print("Ignoring declined transaction", 200)
         return "Ignoring declined transaction", 200
 
-    if transaction_data["account_id"] != MONZO_ACCOUNT_ID:
+    if transaction_data["account_id"] != monzo_account_id:
         print(f"Ignoring transaction for different account ID {data['data']['account_id']}", 200)
         return f"Ignoring transaction for different account ID {data['data']['account_id']}", 200
 
@@ -68,7 +72,11 @@ def main(request: flask.Request) -> Tuple[str, int]:
     transaction: Transaction = MonzoClient.parse_to_transaction(transaction_data)
     print(f"Parsed transaction: {transaction}")
 
-    result = handle_monzo_transaction(transaction)
+    result = handle_monzo_transaction(transaction, ynab_token, ynab_budget_id, ynab_account_id)
     print(f"Result: {result}")
 
     return result
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000))
